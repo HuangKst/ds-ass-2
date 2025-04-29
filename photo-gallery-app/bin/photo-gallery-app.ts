a95c4c86a5ee90e8f7a972871367ae21127a65eb
HuangKst <1300105758@qq.com>
2025-04-29 16:29:23 +0100

    Add S3 bucket and SNS topic for image events

--- photo-gallery-app/bin/photo-gallery-app.ts
+++ photo-gallery-app/bin/photo-gallery-app.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { PhotoGalleryAppStack } from '../lib/photo-gallery-app-stack';
+import { PhotoAppStack } from '../lib/photo-gallery-app-stack';
+
 
 const app = new cdk.App();
-new PhotoGalleryAppStack(app, 'PhotoGalleryAppStack', {
+new PhotoAppStack(app, 'PhotoAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- photo-gallery-app/lib/photo-gallery-app-stack.ts
+++ photo-gallery-app/lib/photo-gallery-app-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
 
-export class PhotoGalleryAppStack extends cdk.Stack {
+export class PhotoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //创建 S3 bucket
+    const imageBucket = new s3.Bucket(this, 'ImageBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PhotoGalleryAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //  创建 SNS Topic
+    const imageTopic = new sns.Topic(this, 'ImageEventTopic', {
+      displayName: 'Image Event Topic',
+    });
+
+    // 输出 Bucket 名 & Topic ARN
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: imageBucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, 'TopicARN', {
+      value: imageTopic.topicArn,
+    });
   }
 }
